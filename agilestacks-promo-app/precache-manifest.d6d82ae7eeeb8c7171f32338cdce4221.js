43eca768c35fc1775f8dc35260f7d906972eb50b
Artem Kudla <kudla.artem@gmail.com>
2020-05-07 05:00:57 +0300

    restore asi-teaser

---
+++ agilestacks-promo-app/precache-manifest.d6d82ae7eeeb8c7171f32338cdce4221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e558090559051aedce3760aa50622ba",
+    "url": "/agilestacks-promo-app/index.html"
+  },
+  {
+    "revision": "12475fb48e3e3ddc69b5",
+    "url": "/agilestacks-promo-app/static/css/main.871f81f4.chunk.css"
+  },
+  {
+    "revision": "12cf9687398e773bf458",
+    "url": "/agilestacks-promo-app/static/js/2.330ce9f5.chunk.js"
+  },
+  {
+    "revision": "bfffb3c6a252b293e2b53fd020e6bf6f",
+    "url": "/agilestacks-promo-app/static/js/2.330ce9f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12475fb48e3e3ddc69b5",
+    "url": "/agilestacks-promo-app/static/js/main.47d68c0c.chunk.js"
+  },
+  {
+    "revision": "7566a28c549dae8f3175",
+    "url": "/agilestacks-promo-app/static/js/runtime-main.daf274b1.js"
+  }
+]);
