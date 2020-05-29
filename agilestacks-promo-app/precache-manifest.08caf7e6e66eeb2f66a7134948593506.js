763a55c7b8f8c4dc692d3965781a839d1ea14750
Artem Kudla <kudla.artem@gmail.com>
2020-05-30 01:32:12 +0300

    asi dolbysound

---
+++ agilestacks-promo-app/precache-manifest.08caf7e6e66eeb2f66a7134948593506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e3001a554a951c68c40b085653ab4f",
+    "url": "/agilestacks-promo-app/index.html"
+  },
+  {
+    "revision": "ef8a6abde8291cffd622",
+    "url": "/agilestacks-promo-app/static/css/main.71950f9c.chunk.css"
+  },
+  {
+    "revision": "694f5e7186e015782fe1",
+    "url": "/agilestacks-promo-app/static/js/2.494a36f9.chunk.js"
+  },
+  {
+    "revision": "ea125bbfbf2edff6408fa39252e34aa5",
+    "url": "/agilestacks-promo-app/static/js/2.494a36f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef8a6abde8291cffd622",
+    "url": "/agilestacks-promo-app/static/js/main.0d310a08.chunk.js"
+  },
+  {
+    "revision": "7566a28c549dae8f3175",
+    "url": "/agilestacks-promo-app/static/js/runtime-main.daf274b1.js"
+  }
+]);
--- agilestacks-promo-app/service-worker.js
+++ agilestacks-promo-app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/agilestacks-promo-app/precache-manifest.454c7107b4dcb985a6996975f3a8063c.js"
+  "/agilestacks-promo-app/precache-manifest.08caf7e6e66eeb2f66a7134948593506.js"
 );
 
 self.addEventListener('message', (event) => {
